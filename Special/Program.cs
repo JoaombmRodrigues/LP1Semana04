b1d15b0c6fc34eba9151298b3451439e685af98c
João <joaombmrodrigues@gmail.com>
2024-03-15 13:24:28 +0000

    Adiciona um metodo recursivo que devolve o n-ésimo número de uma sequência especial

--- Special/Program.cs
+++ Special/Program.cs
@@ -4,9 +4,30 @@ namespace Special
 {
     class Program
     {
+        private static int GetSpecial( int n)
+        {
+            int res;
+            if (n == 0)
+            {
+                res = 0;
+            }
+            
+            else if (n == 1)
+            {
+                res = 1;
+            }
+            else
+            {
+                res = GetSpecial(n-1)+(2*GetSpecial(n-2));
+            }
+
+            return res;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Insert n-th number of the special sequence: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(GetSpecial(n));
         }
     }
 }
